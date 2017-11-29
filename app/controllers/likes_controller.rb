57f17299975372fb5f74acca7d7061498050a20c
fahmi <seichan83@gmail.com>
2017-11-29 17:15:13 +0800

    Added ActionMailer

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,7 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(get_params)
     if @like.save
+      UserMailer.send_like_notification(params[:note_user_id],current_user.id).deliver_later
       redirect_to root_path
     else
       redirect_to root_path
@@ -11,6 +12,7 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.where(user_id: current_user.id, note_id: params[:id]).first
     @like.destroy
+    UserMailer.send_unlike_notification(params[:id],current_user.id).deliver_later
     redirect_to root_path
   end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,15 @@
 class UserMailer < ApplicationMailer
+  def send_like_notification(user_id, other_user_id)
+    byebug
+    @user = User.find(user_id)
+    @other_user = User.find(other_user_id)
+    mail(to: @user.email, subject: "#{@other_user.name} has liked your post!")
+  end
+
+  def send_unlike_notification(user_id, other_user_id)
+    @user = User.find(user_id)
+    @other_user = User.find(other_user_id)
+    mail(to: @user.email, subject: "#{@other_user.name} has unliked your post!")
+  end
+
 end
